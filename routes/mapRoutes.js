1c6b00d4702fa75a91a695ebdd871ea40142514a
agrey tbamanya <agreytibamanya04@gmail.com>
2023-09-18 13:37:56 +0300

    new files added

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -3,10 +3,10 @@ const router =express.Router()
 const { getMap, getMaps, setMap, updateMapdetails, deleteMap } = require("../controllers/mapControllers")
 const {protect} = require("../middleware/authMiddleware")
 //  post and get all maps method have the same root address("/")
-router.route('/').get( protect,getMaps).post(protect,setMap)
+router.route('/').get(getMaps).post(setMap)
 
 
 // getMap, update,delet map details have the same root address
-router.route('/:id').get(protect,getMap).delete(protect,deleteMap).put(protect,updateMapdetails)
+router.route('/:id').get(getMap).delete(protect,deleteMap).put(updateMapdetails)
 
 module.exports = router
