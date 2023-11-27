a433f16e16ffac7ba5a1f11ff49e270c8f0f4fdc
agrey tbamanya <agreytibamanya04@gmail.com>
2023-11-27 20:09:20 +0300

    added files

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		// uri='mongodb+srv://agreytibamanya04:DJs8zNoNYqDlIrin@cluster0.n6pph5r.mongodb.net/boq'
-	//   WIEsJKLFDW5UV3G4
+
     const conn = await mongoose.connect(process.env.DATABASE_NAME, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
